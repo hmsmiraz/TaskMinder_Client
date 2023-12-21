3cd200f3830b74fca42e0486e8a9b681431ee04f
hmsmiraz <hmsmiraz64729@gmail.com>
2023-12-21 12:17:27 +0600

    added navbar and footer

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -1,10 +1,18 @@
 import {
     createBrowserRouter,
   } from "react-router-dom";
+import MainLayout from "../Layouts/MainLayout";
+import Home from "../Pages/Home/Home";
 
   export const router = createBrowserRouter([
     {
       path: "/",
-      element: <div className="text-red-500 text-center">Hello world!</div>,
-    },
+      element: <MainLayout></MainLayout>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        }
+      ]
+    }
   ]);
